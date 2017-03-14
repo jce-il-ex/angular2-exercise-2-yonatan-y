14a18e6970f466a13bdd2ffb38f42fd8ceb39374
yonatan-y <yonatan.yadid1@gmail.com>
2017-03-14 15:39:47 +0200

    components added

--- my-app2/src/app/app.module.ts
+++ my-app2/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { NavItemComponent } from './nav-item/nav-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    NavItemComponent
   ],
   imports: [
     BrowserModule,
